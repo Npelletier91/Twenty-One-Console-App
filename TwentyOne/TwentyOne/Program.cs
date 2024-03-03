fca6c033f4706f9969b0162e28e77bb9952e8f60
Npelletier91 <nicolas.pelletier@yahoo.com>
2024-03-03 10:15:23 -0500

    uncommented out code to make game playable

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -11,26 +11,19 @@ namespace TwentyOne
         static void Main(string[] args)
         {
 
-            //Card card1 = new Card();
-            //Card card2 = card1;
-            //card1.Face = Face.Eight;
-            //card2.Face = Face.King;
-
-            //Console.WriteLine(card1.Face);
 
 
 
-            //Game game = new TwentyOneGame();
-            //game.Players = new List<Player>();
-            //Player player = new Player();
-            //player.Name = "Nic";
-            //game += player; // game = game + player;
-            //game -= player; // game = game - player;
-
+            Game game = new TwentyOneGame();
+            game.Players = new List<Player>();
+            Player player = new Player();
+            player.Name = "Nic";
+            game += player;
+            
 
             Deck deck = new Deck();
 
-            //int count = deck.Cards.Count(x => x.Face == Face.King);
+            int count = deck.Cards.Count(x => x.Face == Face.King);
 
             List<Card> newList = deck.Cards.Where(x => x.Suit == Suit.Clubs).ToList();
 
@@ -41,15 +34,16 @@ namespace TwentyOne
             }
 
 
-            //int timesShuffled = 0;
-            //deck.Shuffle(out timesShuffled, 4); //calling shuffle method (4) times. returning shuffled deck to deck.
+            int timesShuffled = 0;
+            deck.Shuffle(out timesShuffled, 4); //calling shuffle method (4) times. returning shuffled deck to deck.
 
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            //Console.WriteLine("Times Shuffled: {0}", timesShuffled);
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            
+            Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Times Shuffled: {0}", timesShuffled);
 
             Console.ReadLine();
         }
